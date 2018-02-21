b67b14ee4ef20cc0b62a6f3bd3e14d37b2836dc7
Arash Thr <arashThr@outlook.com>
2018-02-21 17:09:09 +0330

    First draft

--- scripts/sliderTags.js
+++ scripts/sliderTags.js
@@ -5,13 +5,13 @@ hexo.extend.tag.register('slides', function(args) {
 
   let name = args[0]
   let len = parseInt(args[1]);
-  let size = args[2];
+  let size = args[2] || '1600x1066';
 
   for (let i = 1; i<=len; i++) {
     pics += `
     <figure class="col-md-5 itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject">
-      <a href="${hexo.config.root}images/details/pics/${name}_${i}.jpg" itemprop="contentUrl" data-size="${size}">
-          <img src="${hexo.config.root}images/details/thumbs/${name}_${i}.jpg" itemprop="thumbnail" alt="Image description" />
+      <a href="${hexo.config.root}${name}/${name}_${i}.jpg" itemprop="contentUrl" data-size="${size}">
+          <img src="${hexo.config.root}${name}/thumbs/${name}_${i}.jpg" itemprop="thumbnail" alt="Image description" />
       </a>
       <!-- <figcaption itemprop="caption description">Image caption  1</figcaption> -->
     </figure>`
